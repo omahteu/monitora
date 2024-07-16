87193938a9256156e691457ed05f9a3ffa7f06d8
matheus <matheus.mmn@hotmail.com>
2024-07-16 10:48:18 -0300

    adição de campo extra

--- src/php/create.php
+++ src/php/create.php
@@ -7,23 +7,15 @@ $db = $database->getConnection();
 
 $registro = new RegistroTrocasHidrometros($db);
 
-
-
-// Recebe os dados do formulário
 $registro->os = $_POST['os'];
 $registro->codigo = $_POST['codigo'];
 
-
-
-// Verifica se os arquivos foram enviados e processa as imagens
 $registro->testada = isset($_FILES['testada']) ? file_get_contents($_FILES['testada']['tmp_name']) : null;
 $registro->hretirado = isset($_FILES['hretirado']) ? file_get_contents($_FILES['hretirado']['tmp_name']) : null;
 $registro->hnovo = isset($_FILES['hnovo']) ? file_get_contents($_FILES['hnovo']['tmp_name']) : null;
 $registro->cavalete = isset($_FILES['cavalete']) ? file_get_contents($_FILES['cavalete']['tmp_name']) : null;
+$registro->solicitacao = isset($_FILES['solicitacao']) ? file_get_contents($_FILES['solicitacao']['tmp_name']) : null;
 
-
-
-// Insere o registro no banco de dados
 if ($registro->create()) {
     echo "Registro criado com sucesso.";
 } else {
--- src/php/read.php
+++ src/php/read.php
@@ -23,7 +23,8 @@ if ($num > 0) {
             "testada" => base64_encode($testada),
             "hretirado" => base64_encode($hretirado),
             "hnovo" => base64_encode($hnovo),
-            "cavalete" => base64_encode($cavalete)
+            "cavalete" => base64_encode($cavalete),
+            "solicitacao" => base64_encode($solicitacao)
         );
         array_push($registros_arr["records"], $registro_item);
     }
